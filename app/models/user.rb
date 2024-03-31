bf31a39336867128eadf95ee53557b6fcaa8a1e4
palomacha100 <palomacha100@gmail.com>
2024-03-31 12:24:55 -0300

    Inclui senha secreta no arquivo de credenciais

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,16 +7,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+         JWT_SECRET = Rails.application.credentials.jwt_secret_key
+
   def self.token_for(user)
     jwt_headers = {exp: 1.hour.from_now.to_i}
-    payload = {id: user.id, email: user.email, role: user.role}
-    JWT.encode payload.merge(jwt_headers), "muito.secreto", "HS256"
+    payload = {id: user.id, email: user.email, role: user.role}   
+    JWT.encode payload.merge(jwt_headers), JWT_SECRET, "HS256"
   end
   
   def self.from_token(t)
-    decoded = JWT.decode t, "muito.secreto", true, {algorithm: 'HS256'}
+    decoded = JWT.decode t, JWT_SECRET, true, {algorithm: 'HS256'}
     user_data = decoded[0].with_indifferent_access
-    User.find(user_data[:id])
+    User.new(email: user_data[:email], id: user_data[:id])
   rescue JWT::ExpiredSignature
     raise InvalidToken.new
   end
