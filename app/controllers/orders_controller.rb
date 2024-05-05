b9c93a127b0256551d57cc04dda5fd35d20f7ce4
palomacha100 <palomacha100@gmail.com>
2024-05-05 12:00:33 -0300

    Cria resposta do pedido através de jbuilder

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,18 @@
-class OrderController < ApplicationController
-    skip_forgery_protection
-    before_action :only_buyers!, :authenticate!
+class OrdersController < ApplicationController
+    skip_forgery_protection only: [:create]
+    before_action :authenticate!, :only_buyers!
+
+    def create
+        @order = Order.new(order_params) { |o| o.buyer = current_user }
+        puts @order.inspect
+        if !@order.save
+            render json: {errors: @order.errors, status: unprocessable_entity}
+        end
+    end
+
+    private
+
+    def order_params
+        params.require(:order).permit([:store_id])
+    end
 end
