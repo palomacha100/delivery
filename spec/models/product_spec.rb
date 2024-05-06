b79423d4c94da73f3928e44107e9c25d87782ce7
palomacha100 <palomacha100@gmail.com>
2024-05-06 12:07:45 -0300

    Valida a presença de colunas de Usuários

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -9,5 +9,21 @@ RSpec.describe Product, type: :model do
         expect(product["title"]). to eq "Macarrão"
         expect(product["price"]). to eq 1.99
     end
+
+    it "validates minimum length of title" do
+      product = Product.create(title: "Ma", price: 1.99, store: store)
+      expect(product).not_to be_valid
+      expect(product.errors[:title]).to include("is too short (minimum is 3 characters)")
+    end
+    it "validates price" do
+      product = Product.create(title: "Macarrão", price: -5, store: store)
+      expect(product).not_to be_valid
+      expect(product.errors[:price]).to include("must be greater than or equal to 0")
+    end
+    it "validates belongs to store" do
+      product = Product.create(title: "Macarrão", price: 2)
+      expect(product).not_to be_valid
+      expect(product.errors[:store]).to include("must exist")
+    end
   end
 end
