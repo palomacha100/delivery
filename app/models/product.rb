dfe9c61c825921ae9982be4791990ac5057334a6
palomacha100 <palomacha100@gmail.com>
2024-05-06 11:10:26 -0300

    Insere validações de nome e preço em produto

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :store
   has_many :orders, through: :order_items
+  validates :name, presence: true, length: {minimum: 3}
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "presence and validation of columns" do
+    let(:store) {Store.create(name: "Bompreço", user: create(:user))}
+    it "checks if the table contains the correct attributes" do
+        product = Product.create(title: "Macarrão", price: 1.99, store: store)
+        expect(product["title"]). to eq "Macarrão"
+        expect(product["price"]). to eq 1.99
+    end
+  end
 end
