543fd85d0bbb65bb54f637c21a214393e518d929
palomacha100 <palomacha100@gmail.com>
2024-05-06 12:09:41 -0300

    Valida se as senhas do usuário coincidem

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,6 +13,11 @@ RSpec.describe User, type: :model do
       expect(user).not_to be_valid
       expect(user.errors[:role]).to include "can't be blank" 
     end
+    it "checks if passwords match" do
+      user = User.create(email: "macarrao@hotmail.com", password: '123456', password_confirmation: '123457', role: :admin)
+      expect(user).not_to be_valid
+      expect(user.errors[:password_confirmation]).to include "doesn't match Password" 
+  end
     
   end
 end
