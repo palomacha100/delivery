c9363cd339d3187a20e69c2f4e6c7ba5f6210081
palomacha100 <palomacha100@gmail.com>
2024-05-06 12:07:45 -0300

    Valida a presença de colunas de Usuários

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "presence and validation of columns" do
+    it "checks if the table contains the correct attributes" do
+      user = User.create(email: "macarrao@hotmail.com", password: '123456', password_confirmation: '123456', role: :admin)
+      expect(user).to be_valid
+      expect(user.email).to eq "macarrao@hotmail.com"
+      expect(user.role).to eq "admin"
+    end
+    
+  end
 end
