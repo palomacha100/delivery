eeda65b570146eab8de5128cdc8e58235178cd89
palomacha100 <palomacha100@gmail.com>
2024-05-06 10:39:09 -0300

    Corrige teste da view de criação de loja

--- spec/views/stores/new.html.erb_spec.rb
+++ spec/views/stores/new.html.erb_spec.rb
@@ -6,8 +6,18 @@ RSpec.describe "stores/new", type: :view do
     user.save
     user
   }
+
+  let(:sellers) do
+    [
+      create(:user),
+      create(:user_two),
+      create(:user_three)
+    ]
+  end
   
   before(:each) do
+    assign(:sellers, sellers)
+    allow(view).to receive(:current_user).and_return(create(:user_admin))
     assign(:store, Store.new(
       name: "MyString",
       user: user
