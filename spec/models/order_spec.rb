55b4449dfd184e5a7c14b29c79c11074e13e2678
palomacha100 <palomacha100@gmail.com>
2024-05-07 10:16:16 -0300

    Valida o erro caso usuário não seja buyer

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) {create(:user)}
+  let(:store) {Store.create(name: "Burguer King", user: user)}
+  describe "checking field validations" do
+    it "an error is expected to be added if the user has an invalid role" do
+      order = Order.create(buyer: user, store: store)
+      expect(order.errors[:buyer]). to include "should be a `user.buyer`"
+    end
+  end
 end
