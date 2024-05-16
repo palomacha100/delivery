c2a65d4a1dca77fab9204e8ee7977f8ca91783f0
palomacha100 <palomacha100@gmail.com>
2024-05-16 15:06:59 -0300

    feat: Implemneta teste para renderização de dados de produto

--- spec/requests/api/products_spec.rb
+++ spec/requests/api/products_spec.rb
@@ -1,50 +1,30 @@
 require "rails_helper"
 
 RSpec.describe "/stores/:store_id/products", type: :request do
-  
+  let(:user) {
+    create(:user)
+  }
 
+  let(:credential) { Credential.create_access(:seller) }
+
+  let(:signed_in) { api_sign_in(user, credential) }
+
+  let(:store) {
+    Store.create(name: 'Burguer King', user: user)
+  }
     describe "GET /show" do
       it "renders a successful response with products data" do
-        get stores_url
-        expect(response). to be_successful
-        expect(response.body).to include "Store 1"
-        expect(response.body).to include "Store 2"
+        Product.create title: "panqueca", price: 5, description: "Saborosa panqueca", category: "almoço",
+        portion: "2 pessoas",
+        store: store
+        get "/stores/#{store.id}/products",
+        headers: {"Accept" => "application/json", "Authorization" => "Bearer #{signed_in["token"]}"}
+        json = JSON.parse(response.body)
+        expect(json["data"][0]["title"]).to eq "panqueca"
+        expect(json["data"][0]["category"]).to eq "almoço"
+        expect(json["data"][0]["price"].to_i).to eq 5
       end
     end
-
-    describe "POST /create" do
-      it "creates a new Store" do
-        store_attributes = {
-          name: "What a great store",
-          user_id: user.id
-        }
-
-        expect {
-          post stores_url, params: {store: store_attributes } 
-      }.to change(Store, :count).by(1)
-
-      expect(Store.find_by(name: "What a great store").user).to eq user
-      end
-    end
-
-  end
-  
-  describe "GET /show" do
-    it "renders a successful response with stores data" do
-      store = Store.create!(
-        name: "New Store",
-        user: user
-        )
-      get "/stores/#{store.id}", 
-      headers: {
-        "Accept" => "application/json", 
-        "Authorization" => "Bearer #{signed_in["token"]}"
-      }
     
-      
-      json = JSON.parse(response.body)
-
-      expect(json["name"]).to eq "New Store"
-    end
-  end
+    
 end
