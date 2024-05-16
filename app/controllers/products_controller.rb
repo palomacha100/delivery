f887ff8c23b6c387ac64223f6e08dcf7a3cc399b
palomacha100 <palomacha100@gmail.com>
2024-05-16 09:50:35 -0300

    feat: Implementa rota de criação de produto
    Cria método create e resposta json para a rota show

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,12 +3,37 @@ class ProductsController < ApplicationController
   before_action :set_product, only: %i[ show edit update destroy ]
   rescue_from User::InvalidToken, with: :not_authorized
   skip_forgery_protection
-  
-    if current_user.admin?
-      redirect_to root_path, notice: "No permission for you"
+
+  def listing
+    if request.format == Mime[:json]
+      if @user && @user.admin?
+        @products = Product.all
+        render json: { message: "Success", data: @products}
+      else
+        render json: { error: "Unauthorized" }, status: :unauthorized
+      end
+    else
+      if !current_user.admin?
+        redirect_to root_path, notice: "No permision for you"
+      else
+        @products = Product.includes(:store)
+      end
     end
-    @products = Product.includes(:store)
   end
+
+  def create
+    @product = Product.new(product_params)
+    respond_to do |format|
+      if @product.save
+        format.html { redirect_to store_url(@product), notice: "Product was successfully created." }
+        format.json { render :show, status: :created, location: @product }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
 
   private
   def product_params
