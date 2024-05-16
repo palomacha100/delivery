d8c6541847eaae5df591520a86455b371e05d1b3
palomacha100 <palomacha100@gmail.com>
2024-05-16 11:22:30 -0300

    feat: Cria action Create e ajusta view de produtos

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,8 @@
-class ProductsController < ApplicationController
+class ProductsController < ApplicationController  
   before_action :authenticate!
-  before_action :set_product, only: %i[ show edit update destroy ]
+
+  skip_forgery_protection 
   rescue_from User::InvalidToken, with: :not_authorized
-  skip_forgery_protection
 
   def listing
     if request.format == Mime[:json]
@@ -18,15 +18,16 @@ class ProductsController < ApplicationController
       else
         @products = Product.includes(:store)
       end
-    end
+    end   
   end
 
   def create
-    @product = Product.new(product_params)
+    @store = Store.find(params[:store_id])  # Captura o store_id da URL
+    @product = @store.products.new(product_params)
     respond_to do |format|
       if @product.save
-        format.html { redirect_to store_url(@product), notice: "Product was successfully created." }
-        format.json { render :show, status: :created, location: @product }
+        format.html { redirect_to store_products_url(@product), notice: "Product was successfully created." }
+        format.json { render :show, status: :created, location: store_products_url(@store, @product) }
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @product.errors, status: :unprocessable_entity }
@@ -34,19 +35,17 @@ class ProductsController < ApplicationController
     end
   end
 
-
   private
-  def product_params
-    required = params.require(:product),
-    permit(:title, :price, :description, :image, :category, :portion)
-  end
 
   def set_product
     @product = Product.find(params[:id])
   end
 
+  def product_params
+    required = params.require(:product).permit(:title, :price, :description, :image, :category, :portion)
+  end
+
   def not_authorized(e)
     render json: {message: "Nope!"}, status: 401
   end
-
 end
