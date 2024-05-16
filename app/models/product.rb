2d59ae8b9dfb65355fc3294305d85030043e1637
palomacha100 <palomacha100@gmail.com>
2024-05-16 08:47:14 -0300

    feat: Adiciona campos na tabela de  produtos
    Adiciona campos de descrição, categoria e porção e atualiza banco

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :store
+  has_one attached: image
   has_many :orders, through: :order_items
   validates :title, presence: true, length: {minimum: 3}
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
