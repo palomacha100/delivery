345bf680a283bf2166f71fae29276cb192f5a1c1
palomacha100 <palomacha100@gmail.com>
2024-05-17 11:12:45 -0300

    feat: Adiciona novos campos a tabela de Store e gera migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_16_113859) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_17_141126) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -84,6 +84,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_16_113859) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "cnpj"
+    t.string "phonenumber"
+    t.string "city"
+    t.string "cep"
+    t.string "state"
+    t.string "neighborhood"
+    t.text "address"
+    t.string "numberadress"
+    t.string "complementadress"
+    t.string "establishment"
     t.index ["user_id"], name: "index_stores_on_user_id"
   end
 
